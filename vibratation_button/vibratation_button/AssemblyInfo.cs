0b6316bcf4fa824e6b684f31bd42ca5096ca268c
Vítor Marques de Castro <vitormcastro@outlook.com>
2020-05-15 20:18:00 -0300

    First Commit

---
+++ vibratation_button/vibratation_button/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using Android.App;
+using Xamarin.Forms.Xaml;
+[assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
---
+++ vibratation_button/vibratation_button/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace vibratation_button
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int time = 500;
+        public MainPage()
+        {
+            InitializeComponent();
+            txtValue.Text = time.ToString();
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                // Or use specified time
+                var duration = TimeSpan.FromSeconds(time);
+                Vibration.Vibrate(duration);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                string message = ex.Message;
+                Toast.MakeText(Android.App.Application.Context, message, ToastLength.Long).Show();
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                Toast.MakeText(Android.App.Application.Context, message, ToastLength.Long).Show();
+            }
+        }
+
+        private void txtValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtValue.Text))
+                try
+                {
+                    int t = Convert.ToInt32(txtValue.Text);
+                    if (t > 0)
+                    {
+                        time = t;
+                    }
+                }
+                finally
+                {
+                    txtValue.Text = time.ToString();
+                }
+        }
+    }
+}
